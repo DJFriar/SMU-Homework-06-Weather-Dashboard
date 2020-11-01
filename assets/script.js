db3f6ec8545805c408f2c9f9b1fe7a8f44433fb0
DJFriar <tommy.craft@icloud.com>
2020-11-01 16:12:22 -0600

    Both api calls work.

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    var APIKey = "&appid=c04d3b0e71450877c96228b5595d876b";
+    var APIKey = "&units=imperial&appid=c04d3b0e71450877c96228b5595d876b";
     var weatherForecastAPIurl = "http://api.openweathermap.org/data/2.5/forecast?q=";
-    var currentWeatherAPIurl = "";
+    var currentWeatherAPIurl = "https://api.openweathermap.org/data/2.5/weather?q=";
 
     // handle the search button click
     $("#searchBtn").on("click", function(event) {
@@ -10,15 +10,24 @@ $(document).ready(function() {
 
         fetchCurrentWeather(inputtedCity);
         fetch5DayForecast(inputtedCity);
+        displayWeatherData();
     });
 
     function fetchCurrentWeather(inputtedCity) {
-        
+        var queryURL = currentWeatherAPIurl + inputtedCity + APIKey;
+        console.log(currentWeatherAPIurl + inputtedCity + APIKey);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+              console.log(response);
+        });
+
         alert("fetching current weather" + inputtedCity);
     };
 
     function fetch5DayForecast(inputtedCity) {
-        
         var queryURL = weatherForecastAPIurl + inputtedCity + APIKey;
         console.log(weatherForecastAPIurl + inputtedCity + APIKey);
 
@@ -27,8 +36,12 @@ $(document).ready(function() {
             method: "GET"
           }).then(function(response) {
               console.log(response);
-          });
+        });
 
         alert("fetching 5-day forecast" + inputtedCity);
+    };
+
+    function displayWeatherData() {
+        $("#mainContent").text("Weather Data Goes Here");
     };
 });
