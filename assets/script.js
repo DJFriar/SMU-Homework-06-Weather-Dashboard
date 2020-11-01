5bdae6ee27a5356179add6bbcefcfb5358cafdd2
DJFriar <tommy.craft@icloud.com>
2020-11-01 16:04:18 -0600

    5 day forecast returns data

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
+    var APIKey = "&appid=c04d3b0e71450877c96228b5595d876b";
+    var weatherForecastAPIurl = "http://api.openweathermap.org/data/2.5/forecast?q=";
+    var currentWeatherAPIurl = "";
 
     // handle the search button click
     $("#searchBtn").on("click", function(event) {
         event.preventDefault();
+        var inputtedCity = $("#searchField").val();
 
-        alert("Search button clicked");
+        fetchCurrentWeather(inputtedCity);
+        fetch5DayForecast(inputtedCity);
     });
 
+    function fetchCurrentWeather(inputtedCity) {
+        
+        alert("fetching current weather" + inputtedCity);
+    };
+
+    function fetch5DayForecast(inputtedCity) {
+        
+        var queryURL = weatherForecastAPIurl + inputtedCity + APIKey;
+        console.log(weatherForecastAPIurl + inputtedCity + APIKey);
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+              console.log(response);
+          });
+
+        alert("fetching 5-day forecast" + inputtedCity);
+    };
 });
